12c29f5c0ee832cd947214ac1836d4af26bc69c1
mrizqiassh18 <mrizqiassh18@gmail.com>
2024-02-11 20:27:21 +0700

    updated loading influencer

--- src/pages/general/InfluencerListPage.jsx
+++ src/pages/general/InfluencerListPage.jsx
@@ -237,7 +237,7 @@ const InfluencerList = () => {
       </div>
 
       {loading ? (
-        <div className="loading flex justify-center items-center">
+        <div className="loading flex justify-center items-center w-full">
           <ColorRing
             visible={true}
             height="250"
